4860a9e8edb7a82bd99b22cd0c7d3f43e11eb00f
Yevhen <leshchenkoyevhen@gmail.com>
2023-03-26 11:43:44 -0400

    add RegistrationScreen

--- App.js
+++ App.js
@@ -1,43 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Image,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
 
 const image = require("./assets/background.png");
-const avatar = require("./assets/avatar.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <View>
-          <Image source={avatar} />
-          <View>
-            <Text>Регистрация</Text>
-          </View>
-          <View>
-            <Text></Text>
-            <TextInput placeholder="Логин"></TextInput>
-          </View>
-          <View>
-            <Text></Text>
-            <TextInput placeholder="Адрес электронной почты"></TextInput>
-          </View>
-          <View>
-            <Text></Text>
-            <TextInput placeholder="Пароль"></TextInput>
-          </View>
-          <TouchableOpacity>
-            <Text>Зарегистрироваться</Text>
-          </TouchableOpacity>
-        </View>
-
+      <ImageBackground
+        source={image}
+        resizeMode="cover"
+        style={styles.backgroundImage}
+      >
+        <RegistrationScreen />
         <StatusBar style="auto" />
       </ImageBackground>
     </View>
@@ -51,8 +26,8 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // justifyContent: "center",
   },
-  image: {
+  backgroundImage: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-end",
   },
 });
